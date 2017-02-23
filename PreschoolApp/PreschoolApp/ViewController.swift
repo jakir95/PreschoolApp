13b601d58a828be29dc6e0d7ab7ada673cec4adf
jakir95 <mj14aba@herts.ac.uk>
2017-02-23 11:27:47 +0000

    Add files via upload

--- PreschoolApp/PreschoolApp/ViewController.swift
+++ PreschoolApp/PreschoolApp/ViewController.swift
@@ -16,23 +16,68 @@ class ViewController: UIViewController {
  
     @IBOutlet weak var numBText: UILabel!
     
+    @IBOutlet weak var questionmark: UILabel!
     
-    
-    
-    
-    
-    
-    
+    let numA = arc4random_uniform(5)
+    let numB = arc4random_uniform(5)
     
 
+    @IBAction func zeroButton(sender: UIButton) {
+        if checkAnswer(0) { /* ToDo*/}
+    }
+    
+    @IBAction func oneButton(sender: UIButton) {
+      if checkAnswer(1) { /* ToDo*/}
+    }
+    
+    @IBAction func twoButton(sender: UIButton) {
+       if checkAnswer(2) { /* ToDo*/}
+    }
+    
+    @IBAction func threeButton(sender: UIButton) {
+        if checkAnswer(3) { /* ToDo*/}
+    }
+    
+    @IBAction func fourButton(sender: UIButton) {
+        if checkAnswer(4) { /* ToDo*/}
+    }
+   
+    @IBAction func fiveButton(sender: UIButton) {
+         if checkAnswer(5) { /* ToDo*/}
+    }
+    
+    @IBAction func sixButton(sender: UIButton) {
+         if checkAnswer(6) { /* ToDo*/}
+    }
+    
+    @IBAction func sevenButton(sender: UIButton) {
+         if checkAnswer(7) { /* ToDo*/}
+    }
+    
+    @IBAction func eightButton(sender: UIButton) {
+         if checkAnswer(8) { /* ToDo*/}
+    }
+    
+    @IBAction func nineButton(sender: UIButton) {
+         if checkAnswer(9) { /* ToDo*/}
+    }
+    
+    
+    
+    func checkAnswer(a: UInt32) ->Bool {
+         let answer = numA + numB
+        if a == answer { return true }
+        else {return false}
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let numA = String(arc4random_uniform(5))
-        let numB = String(arc4random_uniform(5))
         
-        numAText.text = numA
-        numBText.text = numB
+        numAText.text = String(numA)
+        numBText.text = String(numB)
+        
         
     }
 
