05ad714907096dd4dabb8201a7e5033a0415302f
jakir95 <mj14aba@herts.ac.uk>
2017-02-27 11:15:30 +0000

    Add files via upload

--- PreschoolApp/PreschoolApp/ViewController.swift
+++ PreschoolApp/PreschoolApp/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var numBText: UILabel!
     
     @IBOutlet weak var questionmark: UILabel!
+
+    
+
     
     let numA = arc4random_uniform(5)
     let numB = arc4random_uniform(5)
@@ -24,42 +27,53 @@ class ViewController: UIViewController {
 
     @IBAction func zeroButton(sender: UIButton) {
         if checkAnswer(0) { /* ToDo*/}
+        else { wrongAnswerAnimation()}
     }
     
     @IBAction func oneButton(sender: UIButton) {
       if checkAnswer(1) { /* ToDo*/}
+      else { wrongAnswerAnimation()}
     }
     
     @IBAction func twoButton(sender: UIButton) {
        if checkAnswer(2) { /* ToDo*/}
+       else { wrongAnswerAnimation()}
     }
     
     @IBAction func threeButton(sender: UIButton) {
         if checkAnswer(3) { /* ToDo*/}
+        else { wrongAnswerAnimation()}
     }
     
     @IBAction func fourButton(sender: UIButton) {
         if checkAnswer(4) { /* ToDo*/}
+        else { wrongAnswerAnimation()}
     }
    
     @IBAction func fiveButton(sender: UIButton) {
          if checkAnswer(5) { /* ToDo*/}
+         else { wrongAnswerAnimation()}
     }
     
     @IBAction func sixButton(sender: UIButton) {
-         if checkAnswer(6) { /* ToDo*/}
+          if checkAnswer(6) { /* ToDo*/}
+          else { wrongAnswerAnimation()}
+        
     }
-    
+
     @IBAction func sevenButton(sender: UIButton) {
          if checkAnswer(7) { /* ToDo*/}
+         else { wrongAnswerAnimation()}
     }
     
     @IBAction func eightButton(sender: UIButton) {
          if checkAnswer(8) { /* ToDo*/}
+         else { wrongAnswerAnimation()}
     }
     
     @IBAction func nineButton(sender: UIButton) {
          if checkAnswer(9) { /* ToDo*/}
+         else { wrongAnswerAnimation()}
     }
     
     
@@ -70,6 +84,12 @@ class ViewController: UIViewController {
         else {return false}
 
     }
+    func wrongAnswerAnimation(){
+        questionmark.alpha = 0.0
+        questionmark.textColor = UIColor.redColor()
+
+        UIView.animateWithDuration(0.2, delay: 0.1, options: .CurveEaseOut, animations:{self.questionmark.alpha = 1} , completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -77,6 +97,7 @@ class ViewController: UIViewController {
         
         numAText.text = String(numA)
         numBText.text = String(numB)
+        //redQuestionMark.textColor = UIColor.redColor()
         
         
     }
